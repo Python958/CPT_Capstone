2ee956a1db855c08a284472ac445f6778173224c
AaronSalisbury <51139630+AaronSalisbury@users.noreply.github.com>
2019-08-01 03:09:13 -0400

    Restored Random Spawing Script, Fixed Squirrels not animating and killing themselves, Added Static objects to barrels
    Title

--- CPT Capstone/Assets/Scripts/KurtErwin/RandomSpawningForHandPlacement.cs
+++ CPT Capstone/Assets/Scripts/KurtErwin/RandomSpawningForHandPlacement.cs
@@ -3,5 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+public class RandomSpawningForHandPlacement : MonoBehaviour
+{
+    
+    private Collider colliderBox;
+    public GameObject[] decorateObject1;
+    public float decorateObject1Spacing;        //should be how often you want the object to spawn
+    public float decorateObject1ChanceToSpawn;  //0 to 1 how often it should spawn in a space
 
+    public void BuildItems()
+    {
+        var colliderBox = gameObject.GetComponent<Collider>();
+        if(colliderBox != null)
+        {
+            var size = colliderBox.bounds.size;
+
+            if (decorateObject1 != null)
+            {
+                var iterateX = Mathf.Round(size.x / decorateObject1Spacing);
+                var iterateZ = Mathf.Round(size.z / decorateObject1Spacing);
+
+                for(var x = 0; x < iterateX; x++)
+                {
+                    for (var z = 0; z < iterateZ; z++)
+                    {
+                        if(Random.Range(0f,1f) < decorateObject1ChanceToSpawn)
+                        {
+                            var decorateToUse = decorateObject1[Random.Range((int)0, decorateObject1.Length)];
+                            var newDecorate = PrefabUtility.InstantiatePrefab(decorateToUse) as GameObject;
+                            var xOffset = Random.Range(0f, decorateObject1Spacing);
+                            var yOffset = Random.Range(0f, decorateObject1Spacing);
+
+                            var setPos = new Vector3(transform.position.x - size.x / 2f + (decorateObject1Spacing * x) + xOffset, transform.position.y, transform.position.z - size.z / 2f + (decorateObject1Spacing * z) + yOffset);
+                            newDecorate.transform.position = setPos;
+                            newDecorate.transform.rotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+[CustomEditor(typeof(RandomSpawningForHandPlacement))]
+public class RandomSpawnEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        RandomSpawningForHandPlacement myScript = (RandomSpawningForHandPlacement)target;
+        if (GUILayout.Button("Build Object"))
+        {
+            myScript.BuildItems();
+        }
+    }
+}
 
